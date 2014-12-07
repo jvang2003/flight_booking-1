b7d5d1ace26ea9335494e6278d429458b207fe01
Yuri Buerov <yuribuerov@gmail.com>
2014-12-08 01:09:00 +0200

    validations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ActiveRecord::Base
-	has_many :departing_flights, class_name: 'Flight', foreign_key: 'start_airport_id'
-	has_many :arriving_flights, class_name: 'Flight', foreign_key: 'finish_airport_id'
+	has_many :departing_flights, class_name: 'Flight', foreign_key: 'start_airport_id', dependent: :destroy
+	has_many :arriving_flights, class_name: 'Flight', foreign_key: 'finish_airport_id', dependent: :destroy
+
+	validates :code, presence: true, length: {minimum: 3}
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ActiveRecord::Base
   belongs_to :from_airport, class_name: 'Airport', foreign_key: 'start_airport_id'
   belongs_to :to_airport, class_name: 'Airport', foreign_key: 'finish_airport_id'
+
+  validates :start_airport_id, presence: true
+  validates :finish_airport_id, presence: true
 end
